02cf0fd81a5d2734c83d2250e292cbf12e7218e0
Lorenzo Menghini <lorenzo.menghini@studio.unibo.it>
2019-02-14 18:29:50 +0100

    Fixing..

--- js/search.js
+++ js/search.js
@@ -5,12 +5,14 @@ $(document).ready(function() {
     if (ww < 768) {
       $('.content').addClass('changeScreen');
       $('.changeScreen').removeClass('content');
-      document.getElementById("filters").classList.add("tab-pane", "fade");
-      document.getElementById("content").classList.add("tab-pane", "fade", "in", "active");
+      document.getElementById('filters').style.display = "none";
+      //document.getElementById("filters").classList.add("tab-pane", "fade");
+      //document.getElementById("content").classList.add("tab-pane", "fade", "in", "active");
       document.getElementById('myFiltersNav').style.display = "block";
     } else {
-      document.getElementById("filters").classList.remove("tab-pane", "fade");
-      document.getElementById("content").classList.remove("tab-pane", "fade", "in", "active");
+      document.getElementById('filters').style.display = "block";
+      //document.getElementById("filters").classList.remove("tab-pane", "fade");
+      //document.getElementById("content").classList.remove("tab-pane", "fade", "in", "active");
       document.getElementById('myFiltersNav').style.display = "none";
       $('.changeScreen').addClass('content');
       $('.content').removeClass('changeScreen');
