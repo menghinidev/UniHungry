6c2b5d1ae20ac250f4e5ef78abd12b525366fcef
Lorenzo Menghini <lorenzo.menghini@studio.unibo.it>
2019-02-19 00:46:47 +0100

    Updates..

--- js/cart.js
+++ js/cart.js
@@ -22,7 +22,27 @@ $(document).ready(function() {
       document.getElementById('toHide').style.display = "none";
       document.getElementById('toShow').style.display = "block";
       document.getElementById('rem').style.display = "none";
+      $('.logo').removeClass('col-2');
+      $('.logo').addClass('col-4');
+      $('.contenuto').removeClass('col-10');
+      $('.contenuto').addClass('col-8');
+      $('.description').removeClass('col-9');
+      $('.description').addClass('col-12');
+      $('.tofullscreen').removeClass('col-8');
+      $('.tofullscreen').addClass('col-12');
+      $('.change').removeClass('col-3');
+      $('.change').addClass('col-12');
     } else {
+      $('.change').removeClass('col-12');
+      $('.change').addClass('col-3');
+      $('.tofullscreen').removeClass('col-12');
+      $('.tofullscreen').addClass('col-8');
+      $('.description').removeClass('col-12');
+      $('.description').addClass('col-9');
+      $('.logo').removeClass('col-4');
+      $('.logo').addClass('col-2');
+      $('.contenuto').removeClass('col-8');
+      $('.contenuto').addClass('col-10');
       document.getElementById('toHide').style.display = "block";
       document.getElementById('toShow').style.display = "none";
       document.getElementById('rem').style.display = "inline-block";
--- js/search.js
+++ js/search.js
@@ -5,14 +5,26 @@ $(document).ready(function() {
     if (ww < 768) {
       $('.content').addClass('changeScreen');
       $('.changeScreen').removeClass('content');
+      $('.logo').removeClass('col-2');
+      $('.logo').addClass('col-4');
+      $('.contenuto').removeClass('col-10');
+      $('.contenuto').addClass('col-8');
+      $('.description').removeClass('col-9');
+      $('.description').addClass('col-12');
       document.getElementById('filters').style.display = "none";
-      //document.getElementById("filters").classList.add("tab-pane", "fade");
-      //document.getElementById("content").classList.add("tab-pane", "fade", "in", "active");
       document.getElementById('myFiltersNav').style.display = "block";
+      changeDisplay('buttonAppear', 'block');
+      changeDisplay('buttonToHide', 'none');
     } else {
+      $('.description').removeClass('col-12');
+      $('.description').addClass('col-9');
+      $('.logo').removeClass('col-4');
+      $('.logo').addClass('col-2');
+      $('.contenuto').removeClass('col-8');
+      $('.contenuto').addClass('col-10');
+      changeDisplay('buttonAppear', 'none');
+      changeDisplay('buttonToHide', 'block');
       document.getElementById('filters').style.display = "block";
-      //document.getElementById("filters").classList.remove("tab-pane", "fade");
-      //document.getElementById("content").classList.remove("tab-pane", "fade", "in", "active");
       document.getElementById('myFiltersNav').style.display = "none";
       $('.changeScreen').addClass('content');
       $('.content').removeClass('changeScreen');
@@ -25,3 +37,10 @@ $(document).ready(function() {
 
   alterClass();
 })
+
+function changeDisplay(className, property) {
+  var elem = document.getElementsByClassName(className);
+  for (i = 0; i < elem.length; i++) {
+    elem[i].style.display = property;
+  }
+}
