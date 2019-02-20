3938248c620604a172d7e3306445d8ff4611d422
Lorenzo Menghini <lorenzo.menghini@studio.unibo.it>
2019-02-21 00:54:13 +0100

    PadrePio

--- js/home.js
+++ js/home.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     $(".scrolling").click(function(event){
         event.preventDefault();
         $('html,body').animate({scrollTop:$(this.hash).offset().top},
-        Math.abs(window.scrollY - $(this.hash).offset().top) * 1.5);
+        Math.abs(window.scrollY - $(this.hash).offset().top) * 1);
     });
 
 })
