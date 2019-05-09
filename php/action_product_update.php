2c80d4b22c67c51d5fbcea84d4e8409d1d848393
Lorenzo Menghini <lorenzo.menghini2@studio.unibo.it>
2019-05-10 00:10:00 +0200

    TRIED to add or modify products

---
+++ php/action_product_update.php
@@ -0,0 +1,26 @@
+<?php
+require 'db_connect.php';
+require 'secure_func.php';
+sec_session_start();
+
+if(isset($_POST['idprodotto'], $_POST['nome'], $_POST['categoria'], $_POST['descrizione'], $_POST['prezzo'], $_POST['ingredienti'])){
+
+  $nome = "'".mysqli_real_escape_string($mysqli, $_POST['nome'])."'";
+  $categoria = "'".mysqli_real_escape_string($mysqli, $_POST['categoria'])."'";
+  $descrizione = "'".mysqli_real_escape_string($mysqli, $_POST['descrizione'])."'";
+  $ingredienti = "'".mysqli_real_escape_string($mysqli, $_POST['ingredienti'])."'";
+
+  $isUpdate = $mysqli->query("SELECT * FROM prodotti WHERE id_prodotto = ".$_POST['idprodotto']."");
+  if($isUpdate->num_rows != 0){
+    //UPDATE
+    $mysqli->query("UPDATE prodotti SET nome = $nome, categoria = $categoria, descrizione = $descrizione, prezzo = {$_POST['prezzo']}, ingredienti = $ingredienti WHERE id_prodotto = ".$_POST['idprodotto']."");
+
+    header('Location: CONFIRM');
+  } else {
+    //INSERT
+    $mysqli->query("");
+  }
+} else {
+  header('Location: ERROR');
+}
+ ?>
