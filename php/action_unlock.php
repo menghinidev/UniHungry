398c9f95b97efbb38b7faf7319a18694ad578ffd
Samuele Burattini <samuele.burattini@studio.unibo.it>
2019-06-05 14:30:37 +0200

    Revert changes and fixed

--- php/action_unlock.php
+++ php/action_unlock.php
@@ -14,14 +14,29 @@ $stmt = $mysqli->prepare($query);
 $stmt->bind_param("s", $email);
 $stmt->execute();
 
-$newPassword = $_POST['pw'];
+$newPassword = generate_password();
 
 // Crea una chiave casuale
 $random_salt = hash('sha512', uniqid(mt_rand(1, mt_getrandmax()), true));
 $password = $newPassword;
+$newPassword = hash('sha512',$newPassword); //to emulate client hash
 $newPassword = hash('sha512', $newPassword.$random_salt);
 $sql = "UPDATE users  SET `password` = '".$newPassword."', salt = '".$random_salt."' WHERE user_id = $id";
 $mysqli->query($sql);
 
 utenteSbloccato($email, $password);
+
+
+function generate_password($length = 0){
+  $chars =  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.
+            '0123456789';
+
+  $str = '';
+  $max = strlen($chars) - 1;
+
+  for ($i=0; $i < $length; $i++)
+    $str .= $chars[random_int(0, $max)];
+
+  return $str;
+}
 ?>
